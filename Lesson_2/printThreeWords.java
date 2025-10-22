160a8acfac2633744778778cb7a1dbc22959b64b
Lelishna-krab <lelishnina@gmail.com>
2025-10-22 14:35:23 +0500

    Create printThreeWords.java

---
+++ Lesson_2/printThreeWords.java
@@ -0,0 +1,12 @@
+package Lesson_2;
+
+public interface printThreeWords {
+     public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void main(String[] args) {
+    printThreeWords();
+    }
+}
