d881588d783b6a250102f948cdbe8a00ddaf33b4
Lelishna-krab <lelishnina@gmail.com>
2025-10-22 15:51:27 +0500

    Create Main.java

---
+++ Lesson_2/Lesson_2_1/Main.java
@@ -0,0 +1,12 @@
+package Lesson_2.Lesson_2_1;
+public class Main {
+  public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
+
