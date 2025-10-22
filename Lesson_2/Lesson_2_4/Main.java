5d4bad8709affc00ac5625bf3dfe7df6b4240d85
Lelishna-krab <lelishnina@gmail.com>
2025-10-22 15:51:33 +0500

    Create Main.java

---
+++ Lesson_2/Lesson_2_4/Main.java
@@ -0,0 +1,17 @@
+package Lesson_2.Lesson_2_4;
+
+public class Main {
+    public static void compareNumbers() {
+        int a = 26;
+        int b = 18;
+        if (a>=b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+  }  
+
